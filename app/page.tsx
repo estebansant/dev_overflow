f6ed0aecc931b2b9c95de0300e265747fb206bdf
Esteban <estebansant30@gmail.com>
2025-12-30 15:36:28 -0300

    added tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div className='bg-black'>
-      <h1 className='text-red-600'>Welcome!</h1>
+    <div className="bg-black">
+      <h1 className="text-red-600">Welcome!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
