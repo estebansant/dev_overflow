b922fde49f196f2745220c8e6bed129acf73c6a7
Esteban <estebansant30@gmail.com>
2026-01-06 15:07:20 -0300

    auth routes creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import ThemeProvider from "@/context/Theme";
+import ThemeProvider from "@/context/ThemeProvider";
 import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
@@ -33,7 +33,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
