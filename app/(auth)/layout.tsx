75876876138ff13cc9fd8ac8ba49492d3129adfe
Esteban <estebansant30@gmail.com>
2026-01-06 15:54:53 -0300

    signup form ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,26 @@
+import SocialAuthForm from "@/components/forms/SocialAuthForm";
+import Image from "next/image";
 import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="bg-no-repepat flex min-h-screen w-full items-center justify-center bg-[url('/images/auth-light.png')] bg-cover bg-center px-4 py-10 dark:bg-[url('/images/auth-dark.png')]">
+      <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+        <div className="flex-between gap-2">
+          <div className="space-y-2.5">
+            <h1 className="h2-bold text-dark100_light900">Join DevFlow</h1>
+            <p className="paragraph-regular text-dark500_light400">To get your questions answered</p>
+          </div>
+
+          <Image src="images/site-logo.svg" alt="DevFlow logo" width={50} height={50} className="object-contain" />
+        </div>
+
+        {children}
+
+        <SocialAuthForm />
+      </section>
+    </main>
+  );
 };
 
 export default AuthLayout;
