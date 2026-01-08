612b1aacedc91ac7612a6454cbd12e8c750e61ec
Esteban <estebansant30@gmail.com>
2026-01-08 19:30:57 -0300

    right sidebar layout done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,20 @@
 import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="background-light850_dark100 relative">
+    <main className="background-light850_dark100 relative w-full">
       <Navbar />
-      <div className="flex">
+      <div className="flex w-full">
         <LeftSidebar />
 
-        <section className="flec-1 flex min-h-screen flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );
