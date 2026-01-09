263159191706b95ecbc842885c287255462563d4
Esteban <estebansant30@gmail.com>
2026-01-09 18:46:28 -0300

    created homepage structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,27 @@
-import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
 import React from "react";
 
 const page = async () => {
-  const session = await auth();
-
-  console.log(session);
   return (
-    <div className="min-h-[100dvh] bg-black md:h-full">
-      <h1 className="text-red-600">Welcome!</h1>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3" asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search Bar</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
+    </>
   );
 };
 
