01af317a739bbb6c0808a73000937552abfb77b9
Esteban <estebansant30@gmail.com>
2026-01-13 18:01:40 -0300

    api tickets route handlers mockup for practice

---
+++ app/database.ts
@@ -0,0 +1,28 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix Next.js API Route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Add dark mode",
+    status: "closed",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "Improve performance",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 4,
+    name: "Implement SEO and Meta tags",
+    status: "open",
+    type: "feature",
+  },
+];
+
+export default tickets;
